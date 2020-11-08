53ed49e7e8f973356ec763db75d5e78822567f1e
HuyNguyen <huynguyen1212.com>
2020-11-09 01:47:29 +0700

    update

--- Bai 3/index.js
+++ Bai 3/index.js
@@ -1,172 +1,150 @@
+var Arr = Array.from(Array(10).keys()).map((i) => {
+  let DV = Math.ceil(Math.random() * 10),
+    DT = Math.ceil(Math.random() * 10);
+  return {
+    SBD: i,
+    Ten: faker.name.findName(),
+    Tuoi: Math.ceil(Math.random() * 100),
+    Gioitinh: faker.random.boolean() ? "nam" : "nu",
+    Diemvan: DV,
+    Diemtoan: DT,
+    Tong: DV + DT,
+  };
+});
+
+var addInner = "";
+Arr.forEach((value, index) => {
+  addInner +=
+    "<tr>" +
+    "<td>" +
+    value.SBD +
+    "</td>" +
+    "<td>" +
+    value.Ten +
+    "</td>" +
+    "<td>" +
+    value.Tuoi +
+    "</td>" +
+    "<td>" +
+    value.Gioitinh +
+    "</td>" +
+    "<td>" +
+    value.Diemvan +
+    "<td>" +
+    value.Diemtoan +
+    "<td>" +
+    value.Tong +
+    "</tr>";
+});
+
+var Arr2 = [...Arr];
+Arr2 = Arr2.sort((a, b) => a.Diemvan - b.Diemvan);
+var DVan =
+  "<tr>" +
+  "<td>" +
+  Arr2[Arr2.length - 1].SBD +
+  "</td>" +
+  "<td>" +
+  Arr2[Arr2.length - 1].Ten +
+  "</td>" +
+  "<td>" +
+  Arr2[Arr2.length - 1].Tuoi +
+  "</td>" +
+  "<td>" +
+  Arr2[Arr2.length - 1].Gioitinh +
+  "</td>" +
+  "<td>" +
+  Arr2[Arr2.length - 1].Diemvan +
+  "</td>" +
+  "<td>" +
+  Arr2[Arr2.length - 1].Diemtoan +
+  "</td>" +
+  "<td>" +
+  Arr2[Arr2.length - 1].Tong +
+  "</td>" +
+  "</tr>";
+
+var Arr3 = [...Arr];
+Arr3 = Arr3.sort((a, b) => a.Diemtoan - b.Diemtoan);
+var DToan =
+  "<tr>" +
+  "<td>" +
+  Arr3[Arr3.length - 1].SBD +
+  "</td>" +
+  "<td>" +
+  Arr3[Arr3.length - 1].Ten +
+  "</td>" +
+  "<td>" +
+  Arr3[Arr3.length - 1].Tuoi +
+  "</td>" +
+  "<td>" +
+  Arr3[Arr3.length - 1].Gioitinh +
+  "</td>" +
+  "<td>" +
+  Arr3[Arr3.length - 1].Diemvan +
+  "</td>" +
+  "<td>" +
+  Arr3[Arr3.length - 1].Diemtoan +
+  "</td>" +
+  "<td>" +
+  Arr3[Arr3.length - 1].Tong +
+  "</td>" +
+  "</tr>";
+var Arr4 = [...Arr];
+Arr4 = Arr4.sort((a, b) => a.Tong - b.Tong);
+var Toong =
+  "<tr>" +
+  "<td>" +
+  Arr4[Arr4.length - 1].SBD +
+  "</td>" +
+  "<td>" +
+  Arr4[Arr4.length - 1].Ten +
+  "</td>" +
+  "<td>" +
+  Arr4[Arr4.length - 1].Tuoi +
+  "</td>" +
+  "<td>" +
+  Arr4[Arr4.length - 1].Gioitinh +
+  "</td>" +
+  "<td>" +
+  Arr4[Arr4.length - 1].Diemvan +
+  "</td>" +
+  "<td>" +
+  Arr4[Arr4.length - 1].Diemtoan +
+  "</td>" +
+  "<td>" +
+  Arr4[Arr4.length - 1].Tong +
+  "</td>" +
+  "</tr>";
+
 document.getElementById("myDiv").innerHTML =
   "<table id = 'myTable'>" +
   "<tr>" +
   "<th>SBD</th>" +
   "<th>Tên</th>" +
   "<th>Tuổi</th>" +
-  "<th>Giới tính</th>" +
-  "<th>Điểm môn văn</th>" +
-  "<th>Điểm môn toán</th>" +
-  "<th>Tổng điểm</th>" +
+  "<th>Giới thiệu</th>" +
+  "<th>Điểm văn <button onClick= 'sortTable(4)'>Sort</button> <button onClick= 'sortTableReverse(4)'>Sort Reverse</button></th>" +
+  "<th>Điểm toán <button onClick= 'sortTable(5)'>Sort</button> <button onClick= 'sortTableReverse(5)'>Sort Reverse</button></th>" +
+  "<th>Tổng <button onClick= 'sortTable(6)'>Sort</button> <button onClick= 'sortTableReverse(6)'>Sort Reverse</button></th>" +
   "</tr>" +
-  "<tr>" +
-  "<td>01</td>" +
-  "<td>NV A</td>" +
-  "<td>15</td>" +
-  "<td>Nam</td>" +
-  "<td>7</td>" +
-  "<td>8</td>" +
-  "<td>15</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>02</td>" +
-  "<td>NV B</td>" +
-  "<td>15</td>" +
-  "<td>Nam</td>" +
-  "<td>7</td>" +
-  "<td>9</td>" +
-  "<td>16</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>03</td>" +
-  "<td>NV C</td>" +
-  "<td>16</td>" +
-  "<td>Nữ</td>" +
-  "<td>8</td>" +
-  "<td>8</td>" +
-  "<td>16</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>04</td>" +
-  "<td>NV C</td>" +
-  "<td>16</td>" +
-  "<td>Nam</td>" +
-  "<td>7</td>" +
-  "<td>10</td>" +
-  "<td>17</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>05</td>" +
-  "<td>NV D</td>" +
-  "<td>15</td>" +
-  "<td>Nữ</td>" +
-  "<td>9</td>" +
-  "<td>8</td>" +
-  "<td>17</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>06</td>" +
-  "<td>NV E</td>" +
-  "<td>17</td>" +
-  "<td>Nữ</td>" +
-  "<td>8</td>" +
-  "<td>9</td>" +
-  "<td>17</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>07</td>" +
-  "<td>NV F</td>" +
-  "<td>18</td>" +
-  "<td>Nam</td>" +
-  "<td>10</td>" +
-  "<td>10</td>" +
-  "<td>20</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>08</td>" +
-  "<td>NV G</td>" +
-  "<td>16</td>" +
-  "<td>Nữ</td>" +
-  "<td>7</td>" +
-  "<td>8</td>" +
-  "<td>15</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>09</td>" +
-  "<td>NV H</td>" +
-  "<td>17</td>" +
-  "<td>Nam</td>" +
-  "<td>8</td>" +
-  "<td>8</td>" +
-  "<td>16</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<td>10</td>" +
-  "<td>NV I</td>" +
-  "<td>18</td>" +
-  "<td>Nữ</td>" +
-  "<td>9</td>" +
-  "<td>9</td>" +
-  "<td>18</td>" +
-  "</tr>" +
-  "<tr>" +
-  "<th></th>" +
-  "<th></th>" +
-  "<th></th>" +
-  "<th></th>" +
-  "<th><button onClick= 'sortTable(4)'>Sort</button></th>" +
-  "<th><button onClick= 'sortTable(5)'>Sort</button></th>" +
-  "<th><button onClick= 'sortTable(6)'>Sort</button></th>" +
-  "</tr>" +
-  "<tr>" +
-  "<th></th>" +
-  "<th></th>" +
-  "<th></th>" +
-  "<th></th>" +
-  "<th><button onClick= 'sortTableReverse(4)'>Sort Reverse</button></th>" +
-  "<th><button onClick= 'sortTableReverse(5)'>Sort Reverse</button></th>" +
-  "<th><button onClick= 'sortTableReverse(6)'>Sort Reverse</button></th>" +
-  "</tr>" +
+  addInner +
   "</table>" +
-  "<table id='second_table'>" +
+  "</br>" +
+  "<table id = 'tableTop'>" +
   "<tr>" +
-  "<th>Văn cao nhất</th>" +
-  "<th>Toán cao nhất</th>" +
-  "<th>Tổng cao nhất</th>" +
+  "<th>SBD</th>" +
+  "<th>Tên</th>" +
+  "<th>Tuổi</th>" +
+  "<th>Giới thiệu</th>" +
+  "<th>Điểm văn</th>" +
+  "<th>Điểm toán</th>" +
+  "<th>Tổng</th>" +
   "</tr>" +
-  "<tr id = 'topVan'>" +
-  "</tr>" +
-  "<tr id = 'topToan'>" +
-  "</tr>" +
-  "<tr id = 'topTong'>" +
-  "</tr>" +
+  DVan +
+  DToan +
+  Toong +
   "</table>";
-
-// var Arr = [];
-
-// function getData() {
-//   let table, rows, i, x, y, Top;
-//   table = document.getElementById("second_table");
-//   while (true) {
-//     rows = table.rows;
-//     for (i = 1; i < rows.length - 1; i++) {
-//       let tD = rows[i].getElementsByTagName("TD");
-//       console.log(tD);
-//       break;
-//     }
-//   }
-// }
-// getData();
-
-// function onTop(topMon, n) {
-//   var table, rows, i, x, y, Top;
-//   table = document.getElementById("second_table");
-//   while (true) {
-//     rows = table.rows;
-//     for (i = 1; i < rows.length - 1; i++) {
-//       x = rows[i].getElementsByTagName("TD")[n];
-//       y = rows[i + 1].getElementsByTagName("TD")[n];
-//       Top = x;
-//       if (Number(x.innerHTML) < Number(y.innerHTML)) {
-//         Top = y;
-//       }
-//     }
-//   }
-//   document.getElementById("topMon").innerHTML = rows[Top];
-// }
-// onTop("topVan", 4);
-// onTop("topToan", 5);
-// onTop("topTong", 6);
 
 function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch;
