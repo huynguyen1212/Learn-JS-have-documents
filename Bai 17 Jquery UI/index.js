e04aa0737ac5bd603a670ab40cb551f57fb84d5e
HuyNguyen <huynguyen1212.com>
2020-11-11 16:09:42 +0700

    update

--- Bai 17 Jquery UI/index.js
+++ Bai 17 Jquery UI/index.js
@@ -1,3 +1,57 @@
-$(function () {
-    $("#tabs").tabs();
-  });
+function tab(obj) {
+  $("#tabs ul li").removeClass("active");
+  $(obj).addClass("active");
+  var id = $(obj).find("a").attr("href");
+  $(".tab_item").hide();
+  $(id).show();
+}
+$("#tabs ul li").click(function () {
+  tab(this);
+  return false;
+});
+
+$(".easyui-dialog").hide();
+var count = $(".fitem input");
+function newUser() {
+  $(".easyui-dialog").dialog();
+}
+
+$(".save").click(function () {
+  let a = $(".firstname").val();
+  let b = $(".lastname").val();
+  let c = $(".phone").val();
+  let d = $(".email").val();
+  console.log(a, b, c, d);
+  $("#dg").append(
+    "<tbody>" +
+      "<tr>" +
+      "<td>" +
+      a +
+      "</td>" +
+      "<td>" +
+      b +
+      "</td>" +
+      "<td>" +
+      c +
+      "</td>" +
+      "<td>" +
+      d +
+      "</td>" +
+      "</tr>" +
+      "</tbody>"
+  );
+  // a = "";
+  // b = "";
+  // c = "";
+  // d = "";
+});
+
+$(".edit-dialog").hide();
+function editUser() {
+  $(".edit-dialog").dialog();
+}
+
+$(".removeUser").hide();
+function destroyUser() {
+  $(".removeUser").dialog();
+}
