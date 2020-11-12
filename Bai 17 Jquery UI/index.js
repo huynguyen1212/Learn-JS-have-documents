b5747e48a87d2f0a8ef4d0f2740a29aa0d36ebba
HuyNguyen <huynguyen1212.com>
2020-11-13 02:22:53 +0700

    update

--- Bai 17 Jquery UI/index.js
+++ Bai 17 Jquery UI/index.js
@@ -16,40 +16,86 @@ function newUser() {
   $(".easyui-dialog").dialog();
 }
 
-$(".save").click(function () {
-  let a = $(".firstname").val();
-  let b = $(".lastname").val();
-  let c = $(".phone").val();
-  let d = $(".email").val();
-  console.log(a, b, c, d);
-  $("#dg").append(
-    "<tbody>" +
+var Arr = [
+  {
+    firstName: "huy",
+    lastName: "nguyen",
+    Phone: 111234,
+    Email: "huynguyen@sasca.com",
+  },
+];
+function addTable(Arr) {
+  let addInner = "";
+  Arr.forEach((value, index) => {
+    addInner +=
       "<tr>" +
       "<td>" +
-      a +
+      "<input type = 'radio' name = 'user'>" +
+      "</input>" +
       "</td>" +
       "<td>" +
-      b +
+      value.firstName +
       "</td>" +
       "<td>" +
-      c +
+      value.lastName +
       "</td>" +
       "<td>" +
-      d +
+      value.Phone +
       "</td>" +
-      "</tr>" +
-      "</tbody>"
-  );
-  // a = "";
-  // b = "";
-  // c = "";
-  // d = "";
+      "<td>" +
+      value.Email +
+      "</td>" +
+      "</tr>";
+  });
+  $("#main").empty();
+  $("#main").append(addInner);
+}
+$(".save").click(function () {
+  let a = $("#firstname");
+  let b = $("#lastname");
+  let c = $("#phone");
+  let d = $("#email");
+  let Obj = {
+    firstName: a.val(),
+    lastName: b.val(),
+    Phone: c.val(),
+    Email: d.val(),
+  };
+  Arr.push(Obj);
+  addTable(Arr);
+  $(".easyui-dialog").dialog("close");
+  a.val("");
+  b.val("");
+  c.val("");
+  d.val("");
 });
 
 $(".edit-dialog").hide();
-function editUser() {
-  $(".edit-dialog").dialog();
+function tim() {
+  let checkedd = $("#main input");
+  let location = -1;
+  for (let index = 0; index < checkedd.length; index++) {
+    if ($(checkedd[index]).prop("checked")) {
+      location = index;
+    }
+  }
+  return location;
 }
+
+$("#Edit").click(function () {
+  let loca = tim();
+  if (loca != -1) {
+    $("#editfirstname").val(Arr[loca].firstName);
+    $("#editlastname").val(Arr[loca].lastName);
+    $("#editphone").val(Arr[loca].Phone);
+    $("#editemail").val(Arr[loca].Email);
+    $(".edit-dialog").dialog();
+  }
+});
+
+$(".saveedit").click(function () {
+  
+});
 
 $(".removeUser").hide();
 function destroyUser() {
